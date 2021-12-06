121b9e8d8e45c7f8ba82db22989faa12827fb2ee
aimiyinun <dywangxiaowen@126.com>
2021-12-06 14:55:16 +0000

    侧边栏链接

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
-		$this->call(RepliesTableSeeder::class);
-
+        $this->call(RepliesTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
     }
 }
